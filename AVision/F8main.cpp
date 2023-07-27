cebd746d08c49a57a63bc11006eee9219973070e
Vu Truong Giang <giangthedev@outlook.com.vn>
2023-07-27 22:39:05 +0700

    initial code

---
+++ AVision/F8main.cpp
@@ -0,0 +1,155 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+#if defined(__GNUG__) || defined(__MINGW64__)
+#include <sys/stat.h>
+#endif
+#include "F8API.h"
+#include "windows.h"
+#include "webcam-head-tracker.hpp"
+
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <iomanip>
+#include <format>
+#include <thread>
+#include <random>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+class SampleOutputVehicleInfoPlugin
+{
+private:
+    F8MainRibbonTabProxy ribbonTab;
+    F8MainRibbonGroupProxy ribbonGroup;
+    F8MainRibbonButtonProxy ribbonButton, stopBtn;
+    void* p_cbHandle;
+    void* p_stopHandle;
+
+public:
+    SampleOutputVehicleInfoPlugin()
+    {
+        // constructor
+    }
+    ~SampleOutputVehicleInfoPlugin()
+    {
+        // destructor
+    }
+
+    std::atomic<bool> isCapturing;
+
+    std::thread thd;
+
+    void OnButtonRoadInfo1Click()
+    {
+        isCapturing.store(true);
+        thd = std::thread(&SampleOutputVehicleInfoPlugin::TrackHead, this);
+        thd.detach();
+    }
+
+    void OnStopBtnClick()
+    {
+        isCapturing.store(false);
+    }
+
+    void TrackHead()
+    {
+        WebcamHeadTracker tracker(WebcamHeadTracker::Debug_Timing | WebcamHeadTracker::Debug_Window);
+        if (!tracker.initWebcam()) {
+            fprintf(stderr, "No usable webcam found\n");
+            return;
+        }
+        if (!tracker.initPoseEstimator()) {
+            fprintf(stderr, "Cannot initialize pose esimator:\n"
+                "haarcascade_frontalface_alt.xml and shape_predictor_68_face_landmarks.dat\n"
+                "are not where they were when libwebcamheadtracker was built\n");
+            return;
+        }
+        float lastPos[3] = { 0.0f, 0.0f, -1.0f };
+
+        while (isCapturing.load() && tracker.isReady())
+        {
+            tracker.getNewFrame();
+            bool gotPose = tracker.computeHeadPose();
+            if (gotPose) {
+                float pos[3];
+                tracker.getHeadPosition(pos);
+                pos[0] *= 1000.0f;
+                pos[1] *= 1000.0f;
+                pos[2] *= 1000.0f;
+                fprintf(stderr, "position in mm: %+6.1f %+6.1f %+6.1f\n", pos[0], pos[1], pos[2]);
+                if (lastPos[2] >= 0.0f) {
+                    float diff[3] = { pos[0] - lastPos[0], pos[1] - lastPos[1], pos[2] - lastPos[2] };
+                    float dist = std::sqrt(diff[0] * diff[0] + diff[1] * diff[1] + diff[2] * diff[2]);
+                    fprintf(stderr, "distance to last position in mm: %+6.1f\n", dist);
+                }
+                float quaternion[4];
+                tracker.getHeadOrientation(quaternion);
+                float halfAngle = std::acos(quaternion[3]);
+                float axis[3];
+                axis[0] = quaternion[0] / std::sin(halfAngle);
+                axis[1] = quaternion[1] / std::sin(halfAngle);
+                axis[2] = quaternion[2] / std::sin(halfAngle);
+                fprintf(stderr, "orientation: rotated %+4.1f degrees around axis (%+4.2f %+4.2f %+4.2f)\n",
+                    2.0f * halfAngle / (float)M_PI * 180.0f, axis[0], axis[1], axis[2]);
+                lastPos[0] = pos[0];
+                lastPos[1] = pos[1];
+                lastPos[2] = pos[2];
+            }
+        }
+    }
+    void StartProgram()
+    {
+        F8MainFormProxy mainForm = g_applicationServices->GetMainForm();
+        F8MainRibbonProxy ribbonMenu = mainForm->GetMainRibbonMenu();
+        ribbonTab = ribbonMenu->GetTabByName(L"CppAPISamples");
+        if (!Assigned(ribbonTab)) {
+            ribbonTab = ribbonMenu->CreateTab(L"CppAPISamples", 10000);
+            ribbonTab->SetCaption(L"C++ API Samples");
+        }
+        ribbonGroup = ribbonTab->CreateGroup(L"GroupVehicleInfo1", 100);
+        ribbonGroup->SetCaption(L"Head Tracking");
+
+        ribbonButton = ribbonGroup->CreateButton(L"EnableLogger");
+        ribbonButton->SetCaption(L"Start");
+        ribbonButton->SetWidth(120);
+        Cb_RibbonMenuItemOnClick callback = std::bind(&SampleOutputVehicleInfoPlugin::OnButtonRoadInfo1Click, this);
+        p_cbHandle = ribbonButton->SetCallbackOnClick(callback);
+
+        stopBtn = ribbonGroup->CreateButton(L"DisableLogger");
+        stopBtn->SetCaption(L"Stop");
+        stopBtn->SetTop(ribbonButton->GetTop() + ribbonButton->GetHeight() + 6);
+        callback = std::bind(&SampleOutputVehicleInfoPlugin::OnStopBtnClick, this);
+        p_stopHandle = stopBtn->SetCallbackOnClick(callback);
+    }
+    void StopProgram()
+    {
+        ribbonButton->UnsetCallbackOnClick(p_cbHandle);
+        ribbonGroup->DeleteControl(ribbonButton);
+        ribbonTab->DeleteGroup(ribbonGroup);
+        if (ribbonTab->GetRibbonGroupsCount() == 0)
+            g_applicationServices->GetMainForm()->GetMainRibbonMenu()->DeleteTab(ribbonTab);
+    }
+};
+
+SampleOutputVehicleInfoPlugin Plugin;
+
+void StartProgram(void)
+{
+    //User Main Code Starts Here
+    Plugin.StartProgram();
+    //User Main Code Ends Here
+}
+void StopProgram(void)
+{
+    //User Main Code Starts Here
+    Plugin.StopProgram();
+    //User Main Code Ends Here
+}
